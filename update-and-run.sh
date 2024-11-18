49c08b99590e4d51deeb4f9d46baae3ca9404b2a
root <root@pve.chriskolb.local>
2024-11-18 14:59:07 +0100

    update-and-run.sh

---
+++ update-and-run.sh
@@ -0,0 +1,6 @@
+root@pve:~/Smarthome# cp proxmox-v1.sh /usr/local/bin/proxmox-monitor.sh 
+root@pve:~/Smarthome# cp parameters /usr/local/bin/ root@pve:~/Smarthome# dos2unix 
+/usr/local/bin/proxmox-monitor.sh dos2unix: converting file 
+/usr/local/bin/proxmox-monitor.sh to Unix format... root@pve:~/Smarthome# chmod +x 
+/usr/local/bin/proxmox-monitor.sh
+root@pve:~/Smarthome# /usr/local/bin/proxmox-monitor.sh
