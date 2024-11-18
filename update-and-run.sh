6226ef08baf8363c02babfc3d5b912484781702b
Chesterfield-93 <78563725+Chesterfield-93@users.noreply.github.com>
2024-11-18 15:02:25 +0100

    update-and-run

--- update-and-run.sh
+++ update-and-run.sh
@@ -1,6 +1,7 @@
-root@pve:~/Smarthome# cp proxmox-v1.sh /usr/local/bin/proxmox-monitor.sh 
-root@pve:~/Smarthome# cp parameters /usr/local/bin/ root@pve:~/Smarthome# dos2unix 
-/usr/local/bin/proxmox-monitor.sh dos2unix: converting file 
-/usr/local/bin/proxmox-monitor.sh to Unix format... root@pve:~/Smarthome# chmod +x 
+#!/bin/bash
+
+cp proxmox-v1.sh /usr/local/bin/proxmox-monitor.sh 
+cp parameters /usr/local/bin/
+dos2unix /usr/local/bin/proxmox-monitor.sh
+chmod +x /usr/local/bin/proxmox-monitor.sh
 /usr/local/bin/proxmox-monitor.sh
-root@pve:~/Smarthome# /usr/local/bin/proxmox-monitor.sh
