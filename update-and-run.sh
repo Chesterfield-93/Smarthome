cc75ecd98e743a81c8b11d14d4b21875af545a92
Chesterfield-93 <78563725+Chesterfield-93@users.noreply.github.com>
2024-11-18 17:24:07 +0100

    change update script to utilize systemctl

--- update-and-run.sh
+++ update-and-run.sh
@@ -5,4 +5,8 @@ cp proxmox-v1.sh /usr/local/bin/proxmox-monitor.sh
 cp parameters /usr/local/bin/
 dos2unix /usr/local/bin/proxmox-monitor.sh
 chmod +x /usr/local/bin/proxmox-monitor.sh
-/usr/local/bin/proxmox-monitor.sh
+systemctl daemon-reload
+systemctl restart proxmox-monitor.service
+systemctl status proxmox-monitor.service
+
+#/usr/local/bin/proxmox-monitor.sh
