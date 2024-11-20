7adfdb4f5380bfb59b6ee3d625fe2a3264da108d
Chesterfield-93 <78563725+Chesterfield-93@users.noreply.github.com>
2024-11-20 18:12:14 +0100

    rm last time files

--- update-and-run.sh
+++ update-and-run.sh
@@ -6,7 +6,10 @@ cp parameters /usr/local/bin/
 dos2unix /usr/local/bin/proxmox-monitor.sh
 chmod +x /usr/local/bin/proxmox-monitor.sh
 systemctl daemon-reload
-systemctl restart proxmox-monitor.service
+systemctl stop proxmox-monitor.service
+rm /var/lib/proxmox_monitor/last_info_time
+rm /var/lib/proxmox_monitor/last_heartbeat_time
+systemctl start proxmox-monitor.service
 systemctl status proxmox-monitor.service
 
 #/usr/local/bin/proxmox-monitor.sh
