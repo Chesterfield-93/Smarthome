b4afd22b2d94c52a917849671f8bf57b9f14c04c
Chesterfield-93 <78563725+Chesterfield-93@users.noreply.github.com>
2024-11-25 13:34:32 +0100

    update-no-run

---
+++ update-no-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git pull
+cp proxmox-v1.sh /usr/local/bin/proxmox-monitor.sh 
+cp parameters /usr/local/bin/
+dos2unix /usr/local/bin/proxmox-monitor.sh
+chmod +x /usr/local/bin/proxmox-monitor.sh
+systemctl daemon-reload
+systemctl stop proxmox-monitor.service
+rm /var/lib/proxmox_monitor/last_info_time
+rm /var/lib/proxmox_monitor/last_heartbeat_time
+#systemctl start proxmox-monitor.service
+#systemctl status proxmox-monitor.service
+
+#/usr/local/bin/proxmox-monitor.sh
